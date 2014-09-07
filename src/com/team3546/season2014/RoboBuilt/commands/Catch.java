48d7e380386de56b7d280a6b309b7c801b084064
Andrew Dickinson <fortknoxcomputer@gmail.com>
2014-09-07 01:20:56 -0400

    Implemented catch functionality

--- src/com/team3546/season2014/RoboBuilt/commands/Catch.java
+++ src/com/team3546/season2014/RoboBuilt/commands/Catch.java
@@ -8,13 +8,25 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 package com.team3546.season2014.RoboBuilt.commands;
+import com.team3546.season2014.RoboBuilt.RobotSystemsGroup;
+import com.team3546.season2014.RoboBuilt.StatusManager;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Command;
 import com.team3546.season2014.RoboBuilt.Robot;
 /**
- *
+ * Extends the backboard and pickup arm and then retracts them when finished
  */
 public class  Catch extends Command {
+    //Stores the systems this command uses
+    RobotSystemsGroup requiredSystems;
+    //Stores the result given by the status manager so we don't undo what we haven't done
+    boolean executeCommand;
     public Catch() {
+        //Build a profile to describe the usage of this command
+        requiredSystems = new RobotSystemsGroup();
+        //This command just needs to use the backboard
+        requiredSystems.backboardSolenoid.value = StatusManager.uses;
+        requiredSystems.armMovementSolenoid.value = StatusManager.uses;
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
 	
@@ -23,19 +35,32 @@ public class  Catch extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
+        //Check for any conflicts between other commands
+        executeCommand = Robot.statusManager.checkForConflictsAndSetNewStatus(requiredSystems);
+        if (executeCommand) {
+            //Set the position of the backboard and arm movement solenoid to extended
+            Robot.backboard.setBackboardSolenoid(DoubleSolenoid.Value.kForward);
+            Robot.pickupArm.setArmMovementSolenoid(DoubleSolenoid.Value.kForward);
+        }
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
     // Called once after isFinished returns true
     protected void end() {
+        if (executeCommand) {
+            Robot.backboard.setBackboardSolenoid(DoubleSolenoid.Value.kReverse);
+            Robot.pickupArm.setArmMovementSolenoid(DoubleSolenoid.Value.kReverse);
+            Robot.statusManager.doneWithSystems(requiredSystems);
+        }
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
